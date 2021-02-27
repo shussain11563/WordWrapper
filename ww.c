e1a6bfc1a0a9b29b8f8f2b2aa89c363c3f0e753d
Kev Sharma <skevkets@gmail.com>
2021-02-27 11:51:06 -0500

    Refactored algorithm, part 2 remains.

--- ww.c
+++ ww.c
@@ -12,6 +12,19 @@
 #define BUFSIZE 256
 #endif
 
+
+
+int algo(int, int, int);
+
+
+/*
+    parts missing:
+        error checks on program call input
+        determining what the input file and output file is
+        if directory -> a loop to complete the step for all files.
+            -> will need O_CREATE flag to make the outputfile.wrap files.
+
+*/
 int main(int argc, char **argv)
 {
 
@@ -22,25 +35,40 @@ int main(int argc, char **argv)
     int EXIT_STATUS = EXIT_SUCCESS;
     int width = atoi(argv[1]);
 
-    int fileDescriptor = open(argv[2], O_RDONLY);
-    if(fileDescriptor == -1){
-        perror("Invalid File Given.");
+    int inputFD= open(argv[2], O_RDONLY);
+    if(inputFD == -1){
+        perror("Invalid Input File Given.");
         return EXIT_FAILURE;
     }
 
-    /* assuming that correct argv[1] and argv[2] */
-    /* Algorithm #1 */
+    int outputFD = 1;
+
+    // algorithm (width, inputfile fd, outputfile fd)
+    algo(width, inputFD, outputFD);
+
+    close(inputFD);
+    close(outputFD);
+
+    return EXIT_STATUS;
+}
+
+/* Does not concern itself with opening/closing files */
+int algo(int width, int inputFD, int outputFD){
+    int EXIT_STATUS = EXIT_SUCCESS;
+    
+    // inputFD = input File Descriptor to read from.
+    // outputFD = File Descriptor to write to.
+
     strbuf_t sb; 
     sb_init(&sb, 12);
     const char newline[1] = {'\n'};
     const char whitespace[1] = {' '};
     const char dash[1] = {'-'};
     char buffer[BUFSIZE]; 
-    int bytes = read(fileDescriptor, buffer, BUFSIZE);
+    int bytes = read(inputFD, buffer, BUFSIZE);
     int count = 0, newlineDetectedOnce = 0;
 
-    /* Prints column size visible by dashes on screen */
-    for(int w=0; w<width; w++) write(1, dash, 1);
+    for(int w=0; w<width; w++) write(outputFD, dash, 1);
     write(1, newline, 1);
 
     while (bytes > 0) {
@@ -48,7 +76,7 @@ int main(int argc, char **argv)
             char c = buffer[i];
             /* Word longer than column width? */
             if(sb.used > width){ EXIT_STATUS = EXIT_FAILURE;}
-            if(count >= width){write(1, newline, 1); count = 0;}
+            if(count >= width){write(outputFD, newline, 1); count = 0;}
 
             if(c == '\n'){
                 if(!newlineDetectedOnce){
@@ -57,41 +85,41 @@ int main(int argc, char **argv)
                 else{
                     /* Two consecutive newlines found*/
                     if(sb.used <= (width-count)){
-                        write(1, sb.data, sb.used);
+                        write(outputFD, sb.data, sb.used);
                         count += (sb.used + 1);
                         sb_reset(&sb);
-                        write(1, whitespace, 1);
+                        write(outputFD, whitespace, 1);
                     }
                     else{
-                        write(1, newline, 1);
-                        write(1, sb.data, sb.used);
+                        write(outputFD, newline, 1);
+                        write(outputFD, sb.data, sb.used);
                         count = sb.used+1;
                         sb_reset(&sb);
-                        write(1, whitespace, 1);
+                        write(outputFD, whitespace, 1);
                         if(count >= width){
-                            write(1, newline, 1);
+                            write(outputFD, newline, 1);
                             count = 0;
                         }
                     }
-                    write(1, newline, 1);
-                    write(1, newline, 1);
+                    write(outputFD, newline, 1);
+                    write(outputFD, newline, 1);
                     count = 0;
                     newlineDetectedOnce = 0;
                 }
             }
             else if(isspace(c) && sb.used!=0){
                 if(sb.used <= (width-count)){
-                    write(1, sb.data, sb.used);
+                    write(outputFD, sb.data, sb.used);
                     count += (sb.used + 1);
                     sb_reset(&sb);
-                    write(1, whitespace, 1);
+                    write(outputFD, whitespace, 1);
                 }
                 else{
-                    write(1, newline, 1);
-                    write(1, sb.data, sb.used);
+                    write(outputFD, newline, 1);
+                    write(outputFD, sb.data, sb.used);
                     count = sb.used+1;
                     sb_reset(&sb);
-                    write(1, whitespace, 1);
+                    write(outputFD, whitespace, 1);
                 }
 
                 if(newlineDetectedOnce){
@@ -106,21 +134,21 @@ int main(int argc, char **argv)
             }
         }
 
-        bytes = read(fileDescriptor, buffer, BUFSIZE);
+        bytes = read(inputFD, buffer, BUFSIZE);
     }
 
-    /* If the file doesn't end with 'space' we fix */
+
     if(sb.used != 0){
         if(sb.used > (width-count)){
-            write(1, newline, 1);
+            write(outputFD, newline, 1);
         }
 
-        write(1, sb.data, sb.used);
+        write(outputFD, sb.data, sb.used);
         sb_reset(&sb);
     }
 
-    write(1, newline, 1);
-    close(fileDescriptor);
+    write(outputFD, newline, 1);
+
     sb_destroy(&sb);
     return EXIT_STATUS;
 }
