fcf127ae05eb7d23ae4d1db7f086fa50e3da5336
shussain11563 <44513446+shussain11563@users.noreply.github.com>
2021-02-26 19:39:02 -0500

    Testing

---
+++ ww.c
@@ -0,0 +1,20 @@
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+    if(argc!=2)
+    {
+
+    }
+    else if
+    {
+        
+    }
+    else if(
+
+    //add error check for if first argc is 
+
+
+
+}
