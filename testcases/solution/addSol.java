887179e50831d2ec62e395a3a9a29eeb5d8a8329
shussain11563 <44513446+shussain11563@users.noreply.github.com>
2021-03-06 16:48:39 -0500

    Added inital testcases

---
+++ testcases/solution/addSol.java
@@ -0,0 +1,29 @@
+public 
+class 
+AddTwoNumb
+ers {
+
+   public 
+static 
+void 
+main(Strin
+g[] args) 
+{
+        
+      int 
+num1 = 5, 
+num2 = 
+15, sum;
+      sum 
+= num1 + 
+num2;
+
+      
+System.out
+.println("
+Sum of 
+these 
+numbers: 
+"+sum);
+   }
+}
